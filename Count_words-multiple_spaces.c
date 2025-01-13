47dde59935ad610b3a220fcc6a13a995c5658231
priyagoyal25 <112259867+priyagoyal25@users.noreply.github.com>
2025-01-13 11:19:06 +0530

    Count words in sentence having multiple spaces

---
+++ Count_words-multiple_spaces.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int count_words(char sentence[100]){
+    int count = 0;
+    for(int i=0;i<strlen(sentence);i++){
+        if(sentence[i] != ' '){
+            count++;
+            while(sentence[i] != ' '){
+                i++;
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+    char sentence[100] ={'\0'};
+    int count = 0;
+
+    printf("Enter sentence: ");
+    scanf("%[^\n]%*c", sentence);
+
+    count = count_words(sentence);
+
+    printf("%s\n", sentence);
+    printf("Words: %d", count);
+}
